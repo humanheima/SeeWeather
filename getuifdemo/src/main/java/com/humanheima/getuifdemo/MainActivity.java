b38d51816bb4f1f9920001846941f354316746e0
humanheima <15764225790@139.com>
2016-09-20 22:33:29 +0800

    个推as版集成

---
+++ getuifdemo/src/main/java/com/humanheima/getuifdemo/MainActivity.java
@@ -0,0 +1,67 @@
+package com.humanheima.getuifdemo;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.igexin.sdk.PushManager;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_PERMISSION = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        PushManager.getInstance().initialize(this.getApplicationContext());
+        PackageManager pkgManager = getPackageManager();
+        // 读写 sd card 权限非常重要, android6.0默认禁止的, 建议初始化之前就弹窗让用户赋予该权限
+        boolean sdCardWritePermission =
+                pkgManager.checkPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, getPackageName()) == PackageManager.PERMISSION_GRANTED;
+
+
+        // read phone state用于获取 imei 设备信息
+        boolean phoneSatePermission =
+                pkgManager.checkPermission(Manifest.permission.READ_PHONE_STATE, getPackageName()) == PackageManager.PERMISSION_GRANTED;
+
+        if (Build.VERSION.SDK_INT >= 23 && !sdCardWritePermission || !phoneSatePermission) {
+            requestPermission();
+        } else {
+            // SDK初始化，第三方程序启动时，都要进行SDK初始化工作
+            PushManager.getInstance().initialize(this.getApplicationContext());
+        }
+
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_PHONE_STATE}, REQUEST_PERMISSION);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_PERMISSION) {
+            if (grantResults.length == 2 && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                PushManager.getInstance().initialize(this.getApplicationContext());
+            } else {
+                Log.e("GetuiSdkDemo",
+                        "we highly recommend that you need to grant the special permissions before initializing the SDK, otherwise some "
+                                + "functions will not work");
+                PushManager.getInstance().initialize(this.getApplicationContext());
+            }
+        } else {
+            onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        MyGeTuiReceiver.payloadData.delete(0, MyGeTuiReceiver.payloadData.length());
+        super.onDestroy();
+    }
+}
---
+++ getuifdemo/src/main/java/com/humanheima/getuifdemo/MyGeTuiReceiver.java
@@ -0,0 +1,54 @@
+package com.humanheima.getuifdemo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.igexin.sdk.PushConsts;
+
+public class MyGeTuiReceiver extends BroadcastReceiver {
+    /**
+     * 应用未启动, 个推 service已经被唤醒,保存在该时间段内离线消息(此时 GetuiSdkDemoActivity.tLogView == null)
+     */
+    public static StringBuilder payloadData = new StringBuilder();
+
+    public MyGeTuiReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle bundle = intent.getExtras();
+        switch (bundle.getInt(PushConsts.CMD_ACTION)) {
+            case PushConsts.GET_MSG_DATA:
+                // 获取透传数据
+                // String appid = bundle.getString("appid");
+                byte[] payload = bundle.getByteArray("payload");
+
+                String taskid = bundle.getString("taskid");
+                String messageid = bundle.getString("messageid");
+                if (payload != null) {
+                    String data = new String(payload);
+                    //Log.d("GetuiSdkDemo", "receiver payload : " + data);
+                    payloadData.append(data);
+                    payloadData.append("\n");
+                    Log.d("GetuiSdkDemo", "receiver payload : " + payloadData.toString());
+                }
+                break;
+            case PushConsts.GET_CLIENTID:
+                String cid = bundle.getString("clientid");
+                if (!TextUtils.isEmpty(cid)) {
+                    Log.e("cid", cid);
+                }
+                break;
+            case PushConsts.GET_SDKONLINESTATE:
+                boolean online = bundle.getBoolean("onlineState");
+                Log.d("GetuiSdkDemo", "online = " + online);
+                break;
+            default:
+                break;
+        }
+    }
+}
