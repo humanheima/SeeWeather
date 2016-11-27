37085c36246d32bbc5aca3632094aa7ede1680ae
dumingwei <15764225790@139.com>
2016-11-27 21:37:26 +0800

    完善了customviewdemo中的SwitchButton,切换时候添加动画效果

--- customviewdemo/src/main/java/com/humanheima/customviewdemo/ui/SwitchButtonActivity.java
+++ customviewdemo/src/main/java/com/humanheima/customviewdemo/ui/SwitchButtonActivity.java
@@ -3,8 +3,9 @@ package com.humanheima.customviewdemo.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Switch;
 
 import com.humanheima.customviewdemo.R;
 import com.humanheima.customviewdemo.widget.SwitchButton;
@@ -14,12 +15,15 @@ import butterknife.ButterKnife;
 
 public class SwitchButtonActivity extends AppCompatActivity {
 
-    @BindView(R.id.android_switch)
-    Switch androidSwitch;
     @BindView(R.id.my_switch)
     SwitchButton mySwitch;
     @BindView(R.id.activity_switch_button)
     LinearLayout activitySwitchButton;
+    @BindView(R.id.btn_control)
+    Button btnControl;
+    @BindView(R.id.my_switch_selected)
+    SwitchButton mySwitchSelected;
+    private boolean checked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +34,13 @@ public class SwitchButtonActivity extends AppCompatActivity {
             @Override
             public void onChange(boolean isChecked) {
                 Log.e("onChange", "isChecked:" + isChecked);
+            }
+        });
+        btnControl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checked=!checked;
+                mySwitch.setCheck(checked);
             }
         });
     }
--- customviewdemo/src/main/java/com/humanheima/customviewdemo/widget/SwitchButton.java
+++ customviewdemo/src/main/java/com/humanheima/customviewdemo/widget/SwitchButton.java
@@ -11,6 +11,7 @@ import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
 
 import com.humanheima.customviewdemo.R;
 
@@ -38,10 +39,7 @@ public class SwitchButton extends View {
     private int switchRate;
 
     //是否选中
-    private boolean isSelect = false;
-
-    //是否滑动中
-    private boolean isScroll = false;
+    private boolean checked = false;
 
     //是否有动画
     private boolean isAnim = false;
@@ -51,22 +49,26 @@ public class SwitchButton extends View {
 
     private int width = 0;
     private int height = 0;
-
-    //重绘次数
-    private int paintTimes = 0;
-
+    private int defaultWidth;//默认的宽度
     private float buttonRadius = 0;
-    private float animEach = 0;
     private ObjectAnimator objectAnimator;
-    private long aniDuration = 300;
-    private float process;
+    private long aniDuration = 400;//动画时间
+    private float process;//动画的属性
+    private float distance;//thumb移动的距离
 
     public float getProcess() {
         return process;
     }
 
     public void setProcess(float process) {
-        this.process = process;
+        float tp = process;
+        if (tp > 1) {
+            tp = 1;
+        } else if (tp < 0) {
+            tp = 0;
+        }
+        this.process = tp;
+        invalidate();
     }
 
     private OnSwitchChangeListener onSwitchChangeListener;
@@ -83,14 +85,20 @@ public class SwitchButton extends View {
         super(context, attrs, defStyle);
 
         TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.SwitchButton, defStyle, 0);
-        selectColor = typedArray.getColor(R.styleable.SwitchButton_selectColor, Color.GREEN);
+        selectColor = typedArray.getColor(R.styleable.SwitchButton_selectColor, Color.parseColor("#00e676"));
         normalColor = typedArray.getColor(R.styleable.SwitchButton_normalColor, Color.GRAY);
         buttonColor = typedArray.getColor(R.styleable.SwitchButton_buttonColor, Color.WHITE);
         buttonPadding = typedArray.getDimensionPixelSize(R.styleable.SwitchButton_buttonPadding, (int) TypedValue.applyDimension(
                 TypedValue.COMPLEX_UNIT_DIP, 1, getResources().getDisplayMetrics()));
-        switchRate = typedArray.getInteger(R.styleable.SwitchButton_switchRate, 10);
+        checked = typedArray.getBoolean(R.styleable.SwitchButton_isChecked, checked);
+        if (checked) {
+            process = 1f;
+        }
         typedArray.recycle();
         paint = new Paint();
+        defaultWidth = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, getResources().getDisplayMetrics());//默认宽度40dp
+        objectAnimator = ObjectAnimator.ofFloat(this, "process", 0, 0).setDuration(aniDuration);
+        objectAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
         paint.setAntiAlias(true);
     }
 
@@ -102,11 +110,15 @@ public class SwitchButton extends View {
         int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
         int heightSpecSize = MeasureSpec.getSize(heightMeasureSpec);
         if (widthSpecMode == MeasureSpec.AT_MOST && heightSpecMode == MeasureSpec.AT_MOST) {
-            setMeasuredDimension(160, 100);
+            setMeasuredDimension(defaultWidth, (int) (defaultWidth * 0.625));
         } else if (widthSpecMode == MeasureSpec.AT_MOST) {
             setMeasuredDimension((int) (1.6 * heightSpecSize), heightSpecSize);
         } else if (heightSpecMode == MeasureSpec.AT_MOST) {
             setMeasuredDimension(widthSpecSize, (int) (widthSpecSize * 0.625));
+        } else {
+            //精准模式
+            if (heightSpecSize>=widthSpecSize)
+                setMeasuredDimension(widthSpecSize, (int) (widthSpecSize * 0.625));
         }
     }
 
@@ -117,45 +129,37 @@ public class SwitchButton extends View {
             height = getHeight();
             //构建一个矩形用来绘制
             rectF = new RectF(0, 0, width, height);
-
-            //选择按钮半径,以为运算符优先级低
+            //选择按钮半径,移位运算符优先级低
             buttonRadius = height - buttonPadding * 2 >> 1;
-            //每次移动距离为宽度减去按钮的直径和两边的padding除以switchRate
-            animEach = (width - buttonRadius * 2 - buttonPadding * 2) / switchRate;
+            //todo 总共移动的距离是(width - buttonRadius * 2 - buttonPadding * 2)
+            //todo 每次移动的距离process*(width - buttonRadius * 2 - buttonPadding * 2)
+            distance = (width - buttonRadius * 2 - buttonPadding * 2);
         }
-        if (paintTimes < switchRate && isAnim) {
-            paint.setColor(isSelect ? normalColor : selectColor);
+        //TODO 原来的背景色应该逐渐淡去，新的背景色逐渐画出来
+        if (checked) {
+            paint.setColor(selectColor);
             canvas.drawRoundRect(rectF, height / 2, height / 2, paint);
         } else {
             //初始绘制到屏幕上的时候isAnim为false,isSelect为false,绘制圆角矩形的颜色为normalColor
-            paint.setColor(!isSelect ? normalColor : selectColor);
+            paint.setColor(normalColor);
             canvas.drawRoundRect(rectF, height / 2, height / 2, paint);
         }
         paint.setColor(buttonColor);
-        if (isSelect) {
+        if (checked) {
             if (isAnim) {
-                canvas.drawCircle(buttonRadius + buttonPadding + animEach * paintTimes, buttonRadius + buttonPadding, buttonRadius, paint);
+                canvas.drawCircle(buttonRadius + buttonPadding + process * distance, buttonRadius + buttonPadding, buttonRadius, paint);
             } else {
                 canvas.drawCircle(width - buttonRadius - buttonPadding, buttonRadius + buttonPadding, buttonRadius, paint);
             }
         } else {
             if (isAnim) {
-                canvas.drawCircle(width - buttonRadius - buttonPadding - animEach * paintTimes, buttonRadius + buttonPadding, buttonRadius, paint);
+                canvas.drawCircle(buttonRadius + buttonPadding + process * distance, buttonRadius + buttonPadding, buttonRadius, paint);
             } else {
                 //初始绘制thumb到屏幕上
                 canvas.drawCircle(buttonRadius + buttonPadding, buttonRadius + buttonPadding, buttonRadius, paint);
             }
         }
 
-        //移动动画
-        if (paintTimes < switchRate && isAnim) {
-            paintTimes++;
-            invalidate();
-        } else {
-            paintTimes = 0;
-            isAnim = false;
-            isScroll = false;
-        }
     }
 
     @Override
@@ -164,13 +168,12 @@ public class SwitchButton extends View {
             case MotionEvent.ACTION_DOWN:
                 break;
             case MotionEvent.ACTION_UP:
-                if (!isScroll) {
-                    isSelect = !isSelect;
+                if (!objectAnimator.isRunning()) {
                     isAnim = true;
-                    isScroll = true;
-                    postInvalidate();
+                    checked = !checked;
+                    animateToState(checked);
                     if (onSwitchChangeListener != null) {
-                        onSwitchChangeListener.onChange(isSelect);
+                        onSwitchChangeListener.onChange(checked);
                     }
                 }
                 break;
@@ -182,17 +185,32 @@ public class SwitchButton extends View {
         return true;
     }
 
+    private void animateToState(boolean isSelect) {
+        if (objectAnimator == null) {
+            return;
+        }
+        objectAnimator.setDuration(aniDuration);
+        if (isSelect) {
+            objectAnimator.setFloatValues(process, 1f);
+        } else {
+            objectAnimator.setFloatValues(process, 0f);
+        }
+
+        objectAnimator.start();
+    }
+
     public void setOnSwitchChangeListener(OnSwitchChangeListener onSwitchChangeListener) {
         this.onSwitchChangeListener = onSwitchChangeListener;
     }
 
     public boolean isChecked() {
-        return isSelect;
+        return checked;
     }
 
     public void setCheck(boolean select) {
-        isSelect = select;
-        postInvalidate();
+        checked = select;
+        isAnim = true;
+        animateToState(checked);
     }
 
     public int getButtonColor() {
@@ -225,14 +243,6 @@ public class SwitchButton extends View {
 
     public void setSelectColor(int selectColor) {
         this.selectColor = selectColor;
-    }
-
-    public int getSwitchRate() {
-        return switchRate;
-    }
-
-    public void setSwitchRate(int switchRate) {
-        this.switchRate = switchRate;
     }
 
     public interface OnSwitchChangeListener {
